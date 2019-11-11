a34ecb3e5422e462e7b16e385d82f94d266f2b29
Albert LaQue <albertlaque@gmail.com>
2019-11-11 07:16:58 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,11 @@
+#puts phrase until force is equal to 10 
+#increments will be by 1 each phrase 
+
 def using_until
   levitation_force = 6
-  #your code here
-    
+  until levitation_force == 10 
+  puts "Wingardium Leviosa"
+  levitation_force += 1 
+end
 end
 
